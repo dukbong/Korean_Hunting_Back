0295e83b3dc39a136800bc5f9ce4bc511ed72e50
jang <gorae930301@gmail.com>
2024-03-23 13:54:10 +0900

    jwt.secret 분리

--- src/main/java/com/hangulhunting/Korean_Hunting/jwt/TokenProvider.java
+++ src/main/java/com/hangulhunting/Korean_Hunting/jwt/TokenProvider.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -39,6 +40,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
+@PropertySource(value = {"jwt.properties"})
 public class TokenProvider {
 
 	private final Key key;
