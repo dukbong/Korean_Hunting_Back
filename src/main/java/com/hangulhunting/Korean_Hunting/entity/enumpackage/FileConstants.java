caad6d1c7af4055fa790b6ea702d91cc15c81c33
Hyeon Sung Jang <gorae930301@gmail.com>
2024-03-29 17:57:57 +0900

    FileConstants.java 업데이트

--- src/main/java/com/hangulhunting/Korean_Hunting/entity/enumpackage/FileConstants.java
+++ src/main/java/com/hangulhunting/Korean_Hunting/entity/enumpackage/FileConstants.java
@@ -3,6 +3,7 @@ package com.hangulhunting.Korean_Hunting.entity.enumpackage;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+// 삭제 예정 파일을 저장하지 않음
 @AllArgsConstructor
 @Getter
 public enum FileConstants {
